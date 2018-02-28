1362ac30e0cace3e2b47b218a1d8d44a6f3c99db
sarahady1 <36362289+sarahady1@users.noreply.github.com>
2018-02-28 20:51:35 +0200

    Add files via upload

---
+++ forums/main.py
@@ -0,0 +1,10 @@
+import models
+
+member1=models.Member("Zyad Omar",23)
+member2=models.Member("Adam Tarek",20)
+
+post1=models.Post("math","content of the first post")
+post2=models.Post("science","content of the second post")
+
+print (member1.name)
+print(post1.title)
